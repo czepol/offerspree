2dc800c8cf82a695c8f02703e0d0868bf1f8ede9
czepol <szepczynski@gmail.com>
2011-08-02 16:15:00 +0400

    Some updates

--- src/main/scala/com/wpromocji/snippet/Users.scala
+++ src/main/scala/com/wpromocji/snippet/Users.scala
@@ -7,6 +7,7 @@ import net.liftweb.http._
 import net.liftweb.mapper._
 import com.wpromocji.model.{User,Deal,Comment}
 import net.liftweb.util.Helpers._
+import net.liftweb.util.{FieldError}
 import omniauth.{Omniauth}
 import util._
 import S.?
@@ -23,11 +24,11 @@ class Users extends PaginatorSnippet[User] {
 
   override def count = User.count
   
-	override def page = User.findAll(
-	   StartAt(curPage*itemsPerPage),
-	   MaxRows(itemsPerPage),
-	   OrderBy(User.id, Descending)
-	)
+  override def page = User.findAll(
+    StartAt(curPage*itemsPerPage),
+    MaxRows(itemsPerPage),
+    OrderBy(User.id, Descending)
+  )
 	
   override def pageXml(newFirst: Long, ns: NodeSeq): NodeSeq = {
     if(first==newFirst || newFirst < 0 || newFirst >= count)
@@ -53,17 +54,42 @@ class Users extends PaginatorSnippet[User] {
     var email: Any = ""
     var profile: Any = ""
     var submit: Any = ""
+
+    var password = ""
+    var passconf = ""
+
     Omniauth.currentAuthMap match {
       case Full(omni) => {
         provider = omni.get(Omniauth.Provider)
         userData = omni.get(Omniauth.UserInfo)
       }
-      case Empty => NodeSeq.Empty
-      case Failure(_,_,_) => NodeSeq.Empty
+      case Empty => S.redirectTo("/")
+      case Failure(_,_,_) => S.error("Error"); S.redirectTo("/")
     }
     
     provider match {
       case Some(prov) => provider = prov 
+    }
+
+    def validateSignup(user: User): List[FieldError] = user.validate 
+
+    def actionsAfterSignup(theUser: User) {  
+      theUser.validated(true).uniqueId.reset()  
+      theUser.save
+      S.notice(S.??("welcome"))  
+      User.logUserIn(theUser)  
+    }  
+
+    def testSignup() {
+      val theUser: User = User.create
+      validateSignup(theUser) match {  
+        case Nil =>  
+          println("Nie ma bledow")
+          actionsAfterSignup(theUser)  
+          S.redirectTo("/")
+          
+        case xs => S.error(xs); println("Sa bledy") /*; signupFunc(Full(innerSignup _))*/
+      }  
     }
     
     if(provider == "facebook") {
@@ -71,30 +97,30 @@ class Users extends PaginatorSnippet[User] {
         case Some(userData) => {
           userData match {
             case data: Map[String,Any] => {
-              println("To jest Map[String, Any]")
-              println(data.get("Name"))
-              userName  = data.getOrElse("Name", "")
+              userName  = data.getOrElse("Nickname", "")
               firstName = data.getOrElse("FirstName", "")
               lastName  = data.getOrElse("LastName", "")
               locale    = data.getOrElse("Locale", "")
               email     = data.getOrElse("Email", "")
               profile   = data.getOrElse("Profile", "")
               
-              println(userName.toString)
-              println(email)
-              
               bind("user", in, 
-                "username" -> SHtml.text(userName.toString, parm => userName=parm, ("size","55")),
-                "email" -> SHtml.text(email.toString, parm => email=parm, ("size","55")),
-                "submit" -> SHtml.submit(?("submit"), ()=> submit))
+                "username" -> SHtml.text(userName.toString, parm => userName=parm, ("size","35")),
+                "firstname" -> SHtml.text(firstName.toString, parm => firstName=parm, ("size","35")),
+                "lastname" -> SHtml.text(lastName.toString, parm => lastName=parm, ("size", "35")),
+                "locale" -> SHtml.text(locale.toString, parm => locale=parm, ("type", "hidden")),
+                "profile" -> SHtml.text(profile.toString, parm => profile=parm, ("type", "hidden")),
+                "email" -> SHtml.text(email.toString, parm => email=parm, ("size","35")),
+                "password" -> SHtml.password(password, password=_),
+                "passconf" -> SHtml.password(passconf, passconf=_),
+                "submit" -> SHtml.submit(?("submit"), testSignup _))
             }
-            case _ => println("to nie jest"); in
+            case _ => in
           }
         }
-        case None => println("None!"); in
+        case None => in
       }
     } else {
-      println("Nie facebook")
       in
     }
   }
