fdd7f8d852622779d10ef916140acd6fab15ca06
Marcin Szepczyński <szepczynski@gmail.com>
2011-08-08 14:00:26 +0200

    Project Update

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -14,18 +14,19 @@ class LiftProject(info: ProjectInfo) extends DefaultWebProject(info) {
   val junit     = "junit" % "junit" % "4.5" % "test->default"
   val specs     = "org.scala-tools.testing" %% "specs" % "1.6.6" % "test->default"
   
+  val wizard    = "net.liftweb" %% "lift-wizard" % liftVersion % "compile"
   val mapper    = "net.liftweb" %% "lift-mapper" % liftVersion % "compile"
   val textile   = "net.liftweb" %% "lift-textile" % liftVersion % "compile"
   val postgresql= "postgresql" % "postgresql" % "9.0-801.jdbc4" % "compile"
   
-  val databinder_net = "databinder.net repository" at "http://databinder.net/repo"
-  val dispatch = "net.databinder" %% "dispatch-twitter" % "0.7.7"
+  val databinder_net   = "databinder.net repository" at "http://databinder.net/repo"
+  val dispatch_twitter = "net.databinder" %% "dispatch-twitter" % "0.7.7"
   
   /**
    * Maven repositories
    */
   lazy val scalatoolsSnapshots = ScalaToolsSnapshots
 
-  override def temporaryWarPath = "build-jetty"
+  //override def temporaryWarPath = "build-jetty"
   
 }
